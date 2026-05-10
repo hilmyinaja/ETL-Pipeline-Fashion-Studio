1913a3db8ca7c12c47d98b32e3fa1296ee9e99a1
Hilmy Abid Syafi Abiyyu <hilmyabidhasa@gmail.com>
2026-05-10 22:57:32 +0700

    fix: drop na and duplicates line code

--- utils/transform.py
+++ utils/transform.py
@@ -1,30 +1,55 @@
 import pandas as pd
 
 def transform_data(raw_data):
+
     try:
         if isinstance(raw_data, list):
             df = pd.DataFrame(raw_data)
         else:
             df = raw_data.copy()
 
+        df["Title"] = df["Title"].replace("Unknown Product", "Product Item")
+
+        df["Price"] = (
+            df["Price"]
+            .astype(str)
+            .str.replace("$", "", regex=False)
+            .str.replace("Price Unavailable", "NaN", regex=False)
+        )
+        df["Price"] = pd.to_numeric(df["Price"], errors="coerce") * 16000 # Asumsi 1 USD = 16.000 IDR
+
+        df["Rating"] = (
+            df["Rating"]
+            .astype(str)
+            .str.replace(" / 5", "", regex=False)
+            .str.replace("Invalid Rating", "NaN", regex=False)
+            .str.replace("Not Rated", "NaN", regex=False)
+        )
+        df["Rating"] = pd.to_numeric(df["Rating"], errors="coerce")
+
+        df["Colors"] = (
+            df["Colors"].astype(str).str.extract(r"(\d+)").astype("Int64")
+        )
+
+        df["Size"] = (
+            df["Size"]
+            .astype(str)
+            .str.replace("Size: ", "", regex=False)
+            .str.strip()
+        )
+        df["Gender"] = (
+            df["Gender"]
+            .astype(str)
+            .str.replace("Gender: ", "", regex=False)
+            .str.strip()
+        )
+
+        df = df.dropna(subset=["Price", "Rating"])
         df = df.drop_duplicates()
 
-        df = df.dropna()
-
-        df = df[
-            (df['Title'] != 'Unknown Product') & 
-            (df['Price'] != 'Price Unavailable') & 
-            (~df['Rating'].str.contains('Invalid|Not', na=False))
-        ]
-
-        df['Price'] = df['Price'].str.replace('$', '', regex=False).astype(float) * 16000 # Asumsi 1 USD = 16,000 IDR
-
-        df['Rating'] = df['Rating'].str.replace(' / 5', '', regex=False).astype(float)
-
-        df['Colors'] = df['Colors'].str.extract(r'(\d+)').astype(int)
-
-        df['Size'] = df['Size'].str.replace('Size: ', '', regex=False).str.strip()
-        df['Gender'] = df['Gender'].str.replace('Gender: ', '', regex=False).str.strip()
+        df["Price"] = df["Price"].astype(float)
+        df["Rating"] = df["Rating"].astype(float)
+        df["Colors"] = df["Colors"].astype(int)
 
         return df
 
